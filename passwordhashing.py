ac677fc714650cb99c0b4149c3b2accb301ae7f5
DJOUMESSI2004 <wilfridndongmo@gmail.com>
2023-09-11 13:27:12 +0200

    the password hashing file is created

---
+++ passwordhashing.py
@@ -0,0 +1,6 @@
+# bcrypt (Password Hashing):
+
+# Bcrypt is a specialized hash function designed for securely hashing passwords.
+# It includes salting and multiple iterations to make brute-force attacks more difficult.
+# You can use the bcrypt library in Python for bcrypt hashing:
+
