fc1d9ed73ea20eb4c9811913f81d1f9f3e4429e6
DJOUMESSI2004 <wilfridndongmo@gmail.com>
2023-09-11 14:20:07 +0200

    function to hash password added

--- passwordhashing.py
+++ passwordhashing.py
@@ -4,3 +4,13 @@
 # It includes salting and multiple iterations to make brute-force attacks more difficult.
 # You can use the bcrypt library in Python for bcrypt hashing:
 
+import bcrypt
+
+import bcrypt
+
+def PassHash(password):
+    password_bytes = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    print(hashed_password.decode('utf-8'))
+
+PassHash('password')
